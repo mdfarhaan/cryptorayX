03dfd2d173443a69f958ebbb39a6d71d505ef3f7
Farhaan <60516398+mdfarhaan@users.noreply.github.com>
2021-10-11 22:37:41 +0530

    Update cryptorayX.py

--- cryptorayX.py
+++ cryptorayX.py
@@ -4,13 +4,12 @@ from currency_converter import CurrencyConverter
 url = "https://api.wazirx.com/api/v2/tickers"
 response = dict(requests.get(url).json())
 
-class cryptoflash :
+class cryptorayX :
 
     def price(self,coin,currency):
         
         try:
             currencyObject = CurrencyConverter()
-            
             crypto = coin.lower() + "inr"
             currentPrice = float(response[crypto]["last"])
             try:
